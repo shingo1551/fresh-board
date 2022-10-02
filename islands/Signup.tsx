89c4db480e2d436175b54f9019b9c59bf186ff56
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-10-02 19:21:49 +0900

    validation

--- islands/Signup.tsx
+++ islands/Signup.tsx
@@ -1,45 +1,27 @@
-import { ComponentChildren } from "preact";
-import { useRef } from "preact/hooks";
-import { signal } from "@preact/signals";
-
+import { useRef, useState } from "preact/hooks";
 import { fetchCors } from "../shared/fetch.ts";
-
-const err = signal({
-  error: null,
-  email: false,
-  pass1: false,
-  pass2: false,
-  disabled: true,
-});
 
 export default function Signup() {
   const email = useRef<HTMLInputElement>(null);
   const passwd1 = useRef<HTMLInputElement>(null);
   const passwd2 = useRef<HTMLInputElement>(null);
 
-  const onEmail = () => {
-    err.value.email = !email;
-    onPasswd1();
-  };
-
-  const onPasswd1 = () => {
-    err.value.pass1 = !passwd1.current?.value;
-    onPasswd2();
-  };
-
-  const onPasswd2 = () => {
-    err.value.pass2 = passwd1.current?.value !== passwd2.current?.value;
-    buttonDisabled();
-  };
+  const [msgError, setError] = useState("");
+  const [msgEmail, setEmail] = useState("");
+  const [msgPass1, setPass1] = useState("");
+  const [msgPass2, setPass2] = useState("");
+  const [disabled, setDisabled] = useState(true);
 
   const buttonDisabled = () => {
-    const value = err.value;
-    err.value = {
-      ...value,
-      error: null,
-      disabled: value.email || value.pass1 || value.pass2 ||
-        !email.current?.value,
-    };
+    setEmail(!email.current?.value ? "必須入力です" : "");
+    setPass1(!passwd1.current?.value ? "必須入力です" : "");
+    setPass2(
+      passwd1.current?.value !== passwd2.current?.value ? "パスワードが一致しません" : "",
+    );
+    setDisabled(
+      !email.current?.value || !passwd1.current?.value ||
+        passwd1.current?.value !== passwd2.current?.value,
+    );
   };
 
   const onSignUp = async (evt: Event) => {
@@ -53,7 +35,7 @@ export default function Signup() {
       await fetchCors("sign-up", "post", body);
       location.href = "/signin";
     } catch (e) {
-      err.value = { ...err.value, error: e };
+      setError(e);
     }
   };
 
@@ -61,37 +43,33 @@ export default function Signup() {
     <form class="signup">
       <h1>Sign up</h1>
       <div>
-        <Error>{err.value.error}</Error>
+        <span class="error">{msgError}</span>
         <input
           ref={email}
-          onInput={onEmail}
+          onInput={buttonDisabled}
           autocomplete="username"
           placeholder="jane@example.com"
         />
-        {/* <span class='error'>This field is required.</span> */}
+        <span class="error">{msgEmail}</span>
         <input
           ref={passwd1}
-          onInput={onPasswd1}
+          onInput={buttonDisabled}
           type="password"
           autocomplete="new-password"
           placeholder="password"
         />
-        {/* <span class='error'>This field is required.</span> */}
+        <span class="error">{msgPass1}</span>
         <input
           ref={passwd2}
-          onInput={onPasswd2}
+          onInput={buttonDisabled}
           type="password"
           autocomplete="new-password"
           placeholder="password confirm"
         />
-        {/* <span class='error'>This field is required.</span> */}
+        <span class="error">{msgPass2}</span>
       </div>
       <hr />
-      <button onClick={onSignUp} disabled={err.value.disabled}>Sign up</button>
+      <button onClick={onSignUp} disabled={disabled}>Sign up</button>
     </form>
   );
-}
-
-function Error(props: { children: ComponentChildren }) {
-  return props.children ? <div>{props.children}</div> : null;
 }
