d3cb3869b39217d38a40be66d361005a480cbae8
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-10-21 00:49:37 +0900

    deno fmt

--- islands/Signup.tsx
+++ islands/Signup.tsx
@@ -3,37 +3,42 @@ import { fetchCors } from "../shared/fetch.ts";
 
 export default function Signup() {
   const [error, setError] = useState("");
-  const [email, setEmail] = useState({ value: '', error: '', dirty: false });
-  const [pass1, setPass1] = useState({ value: '', error: '', dirty: false });
-  const [pass2, setPass2] = useState({ value: '', error: '', dirty: false });
+  const [email, setEmail] = useState({ value: "", error: "", dirty: false });
+  const [pass1, setPass1] = useState({ value: "", error: "", dirty: false });
+  const [pass2, setPass2] = useState({ value: "", error: "", dirty: false });
 
   const onEmail = (e: Event) => {
     const value = (e.target as HTMLInputElement).value;
-    setEmail({ value, error: value ? '' : "必須入力です", dirty: true });
-  }
+    setEmail({ value, error: value ? "" : "必須入力です", dirty: true });
+  };
 
   const onPass1 = (e: Event) => {
     const value = (e.target as HTMLInputElement).value;
-    setPass1({ value, error: value ? '' : "必須入力です", dirty: true });
+    setPass1({ value, error: value ? "" : "必須入力です", dirty: true });
     _setPass2(value, pass2.value, pass2.dirty);
-  }
+  };
 
   const onPass2 = (e: Event) => {
-    _setPass2(pass1.value, (e.target as HTMLInputElement).value, true)
-  }
+    _setPass2(pass1.value, (e.target as HTMLInputElement).value, true);
+  };
 
   const _setPass2 = (value1: string, value2: string, dirty: boolean) => {
-    if (dirty)
-      setPass2({ value: value2, error: value1 === value2 ? '' : "パスワードが一致しません", dirty });
-  }
+    if (dirty) {
+      setPass2({
+        value: value2,
+        error: value1 === value2 ? "" : "パスワードが一致しません",
+        dirty,
+      });
+    }
+  };
 
-  const isValid = (state: { value: string, error: string, dirty: boolean }) => {
+  const isValid = (state: { value: string; error: string; dirty: boolean }) => {
     return !state.error && state.dirty;
   };
 
   const disabled = () => {
     let valid = true;
-    [email, pass1, pass2].forEach(state => valid &&= isValid(state));
+    [email, pass1, pass2].forEach((state) => valid &&= isValid(state));
     return !valid;
     // return !(isValid(email) && isValid(pass1) && isValid(pass2));
   };
