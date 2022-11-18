446fa22682fb13e038cc0b415e912429870a35a3
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-11-18 21:30:47 +0900

    info

--- routes/api/info.ts
+++ routes/api/info.ts
@@ -2,7 +2,7 @@ import { HandlerContext } from "$fresh/server.ts";
 
 export const handler = (_req: Request, _ctx: HandlerContext): Response => {
   const body = {
-    "Deno.version": Deno.version,
+    // "Deno.version": Deno.version,
     // "osRelease": Deno.osRelease(),
     // "memoryUsage": Deno.memoryUsage(),
     // "hostname": Deno.hostname(),
