d59fb9e75c8f683bf9a6b1cee4b314ef8efc98e2
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-11-18 21:25:51 +0900

    info: api

--- shared/info.ts
+++
@@ -1,8 +0,0 @@
-export default function () {
-  console.log("Deno.version", Deno.version);
-  console.log("osRelease", Deno.osRelease());
-  console.log("memoryUsage", Deno.memoryUsage());
-
-  console.log("hostname", Deno.hostname());
-  console.log("systemMemoryInfo", Deno.systemMemoryInfo());
-}
